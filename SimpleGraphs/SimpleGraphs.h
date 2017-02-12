03e4a94f255e1a47098027f5f017a5e27196ab38
Taillook <jousoku@gmail.com>
2017-02-12 15:52:22 +0900

    first

---
+++ SimpleGraphs/SimpleGraphs.h
@@ -0,0 +1,19 @@
+//
+//  SimpleGraphs.h
+//  SimpleGraphs
+//
+//  Created by Taillook on 2017/02/12.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleGraphs.
+FOUNDATION_EXPORT double SimpleGraphsVersionNumber;
+
+//! Project version string for SimpleGraphs.
+FOUNDATION_EXPORT const unsigned char SimpleGraphsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleGraphs/PublicHeader.h>
+
+
---
+++ SimpleGraphs/SimpleGraphs.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleGraphs.swift
+//  SimpleGraphs
+//
+//  Created by Taillook on 2017/02/12.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class SimpleGraphs{
+    public class LineGraph{
+        public class func hello(){
+            print("hello")
+        }
+    }
+}
